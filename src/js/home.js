668619457c2e98896ac346d7d0dbb892e85e58e6
Nanami <anhdao11a5cmb1920@gmail.com>
2023-10-16 10:47:13 +0700

    fix theo y nam

--- src/js/home.js
+++ src/js/home.js
@@ -1,1 +1,38 @@
-// khong biet gi het...
+$(document).ready(function () {
+    $('.list-best-seller').slick({
+        dots: true,
+        prevArrow: '<i class="fa-solid fa-angle-left"></i>',
+        nextArrow: '<i class="fa-solid fa-angle-right"></i>',
+        infinite: false,
+        speed: 300,
+        slidesToShow: 5,
+        slidesToScroll: 5,
+        responsive: [
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 5,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            }
+        ]
+    });
+});
+
+// sildeshow of best seller - greatest hots *******************
